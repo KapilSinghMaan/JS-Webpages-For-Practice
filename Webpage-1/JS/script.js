a7c4f97595cad92f70b745918e6a2299b83e0318
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-26 21:58:58 +0530

    Javascript

---
+++ Webpage-1/JS/script.js
@@ -0,0 +1,25 @@
+// Printing message in console 
+
+// console.log("Hello world!");
+// console.log('Printing message in single quotes');
+// console.log(`Printing message in back ticks`);
+
+// Printing message by window alert 
+
+// window.alert("Hi");
+// window.alert('window');
+// window.alert(`alert`);
+
+// Printing message using html 
+
+document.getElementById("heading").textContent = "Printing using 'h1' html tag";
+document.getElementById("para").textContent = "This is a paragraph";
+
+
+// This is a single line Comment 
+
+/* This 
+is 
+a 
+multi-line 
+comment */
