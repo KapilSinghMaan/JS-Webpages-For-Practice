077c510fe9279630db21ac7286d104ce08701233
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-27 13:32:02 +0530

    Javascript

---
+++ Webpage-2/JS/script.js
@@ -0,0 +1,14 @@
+// Variable = A container that stores a value. Behaves as if it were the value it contains.
+
+// string 
+let fullname="Kapil Singh Maan";
+
+// Number
+let age = 21;
+
+// Boolean
+let inSchool = false;
+
+document.getElementById("name").textContent=`Your name is ${fullname}`;
+document.getElementById("age").textContent=`Your age is ${age}`;
+document.getElementById("edu").textContent=`You are in school : ${inSchool}`;
