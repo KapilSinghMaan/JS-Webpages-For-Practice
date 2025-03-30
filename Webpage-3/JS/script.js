1848929c7b3ac03a88497574be2f92e7bc0e54d1
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-30 19:25:21 +0530

    Javascript

---
+++ Webpage-3/JS/script.js
@@ -0,0 +1,62 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - * /)
+
+let num = 50;
+
+// normal arithmetic operations
+
+// num = num + 1;
+// console.log(num);
+
+// num = num - 1;
+// console.log(num);
+
+// num = num * 5;
+// console.log(num);
+
+// num = num / 3;
+// console.log(num);
+
+// num = num ** 2;
+// console.log(num);
+
+// num = num % 4;
+// console.log(num);
+
+
+
+// augmented arithmetic operations
+
+num += 1;
+console.log(num);
+
+num -= 1;
+console.log(num);
+
+num *= 2;
+console.log(num);
+
+num /= 4;
+console.log(num);
+
+num %= 3;
+console.log(num);
+
+num **= 2;
+console.log(num);
+
+
+// Increment and Decrement 
+
+// num++;
+// num--;
+
+
+// Precedence of Arithmetic Operators
+
+/*
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
