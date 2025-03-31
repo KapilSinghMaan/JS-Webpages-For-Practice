d7371a8303cc84273e4483343f3b2f64c762c309
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-31 20:40:00 +0530

    Javascript

---
+++ Webpage-5/JS/script.js
@@ -0,0 +1,28 @@
+// Type Conversion in Javascript
+
+// Type conversion = change the datatype of a value to another(strings, numbers, booleans)
+
+
+// EXAMPLE 1
+
+// let age = window.prompt("How old are you?");
+// age = Number(age);                                   ((Run this example with and without this line))
+// age+=1;
+
+// console.log(age, typeof age);
+
+
+
+// -------------- EXAMPLE 2 --------------
+
+let x = "pizza";    // try with other like   = ""    = "0"  = 0   just with declaration
+let y = "pizza";    // try with other like   = ""    = "0"  = 0   just with declaration
+let z = "pizza";    // try with other like   = ""    = "0"  = 0   just with declaration
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
