a5f465e0fa9c2aa0157149758415598b63072eb0
KapilSinghMaan <kapilmaan369@gmail.com>
2025-04-14 20:58:27 +0530

    Javascript

---
+++ Webpage-8/JS/script.js
@@ -0,0 +1,47 @@
+// Math = built-in object that provides a collection of properties and methods
+
+
+// console.log(Math.E);
+// console.log(Math.PI);
+
+let x = 3.1258;
+let y = 2;
+let z;
+
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.round(x);
+// z = Math.trunc(x);
+// console.log(z);
+
+// x = 16;
+// z = Math.pow(x, y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// console.log(z);
+
+// x = 1;
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// console.log(z);
+
+// x = -159;
+// z = Math.abs(x);
+// console.log(z);
+
+// x = -3;
+// console.log(Math.sign(x));
+
+// x = 3;
+// console.log(Math.sign(x));
+
+x = 320;
+y = 87;
+z = 9090;
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(max);
+console.log(min);
