082b10bb0441f4792c7fb80a27c858936c77495e
KapilSinghMaan <kapilmaan369@gmail.com>
2025-04-19 15:06:46 +0530

    Javascript

---
+++ Webpage-17/JS/script.js
@@ -0,0 +1,16 @@
+// logical operators =  used to combine or manipulate Boolean values 
+//                                    (true or false)
+
+//                      AND = &&
+//                      OR  = ||
+//                      NOT = !
+
+
+let temp = 25;
+
+if (temp > 20 && temp < 40) {
+    console.log("The Weather is GOOD");
+}
+else {
+    console.log("The Weather is BAD");
+}
