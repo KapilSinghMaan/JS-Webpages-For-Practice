e2611a1238c5b20cf62107e559f88464d72f15f8
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-03 23:29:05 +0530

    Javascript

--- Webpage-31/JS/script.js
+++ Webpage-31/JS/script.js
@@ -4,9 +4,11 @@ start(stop);
 
 function start(Callback) {
     console.log(`Hello`);
+    console.log(`start function`);
     Callback();
 }
 
 function stop() {
     console.log(`GoodBye`);
+    console.log(`stop function`);
 }
